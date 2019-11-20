ed2c0c4b7949c258978a1cd37d462b40e1ce5f30
asuratos <asuratos@users.noreply.github.com>
2019-11-20 13:51:17 +0900

    Minor cleanups to boundary definitions

--- tcod_tutorial/map_objects/rooms.py
+++ tcod_tutorial/map_objects/rooms.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# make hallway class maybe
+
 class Room:
     def __init__(self, hallwaychance = 0.75):
         self.spaces = np.array([[0,1]])
@@ -13,15 +15,30 @@ class Room:
                            self.mirror_horizontal,        
                            self.mirror_vertical]
 
-    def add_hallway(self, maxlen = 5):
+    def get_neighbors(self, point):
+        return point + np.array([[1,0],
+                                 [-1,0],
+                                 [0,1],
+                                 [0,-1]])
 
+    def get_bounds(self, space = self.spaces):
+        bounds = []
+        for point in space:
+            for neighbor in self.get_neighbors(point):
+                if not np.any((space[:] == neighbor).all(1)):
+                    bounds.append(neighbor)
+        return bounds
+    
+    def update_boundary(self):
+        pass
+        
+    def add_hallway(self, maxlen = 5):
         for _ in range(maxlen):
             if np.random.rand() < self.hallwaychance:
                 self.halllength += 1
-                self.spaces = np.append(self.spaces, [[0,self.halllength]], axis = 0)
-                if self.halllength == 1:
-                    self.boundary = np.append(self.boundary, [[-1, self.halllength],
-                                                            [1, self.halllength]], axis = 0) 
+                self.spaces = np.vstack((self.spaces, [[0,self.halllength]]))
+                self.boundary = np.vstack((self.boundary, [[-1, self.halllength],
+                                                           [1, self.halllength]])) 
             else:
                 break
 
@@ -61,13 +78,17 @@ class RoomRect(Room):
         self.generate_body(self.w, self.h)
 
     def generate_body(self, w, h):
-        body = [[x,y] for x in range(w) for y in range(h)]
-        bounds = [[x,y] for x in range(-1, w) for y in range(-1, h)]
+        body = [[x,y] for x in range(h) for y in range(w)]
+        bounds = [[x,y] for x in range(-1, h+1) for y in range(-1, w+1)]
         walls = [wall for wall in bounds if wall not in body]
 
         shift = np.array([-np.random.randint(self.w), self.halllength])
         body += shift
-        self.spaces = np.append(self.spaces, body, axis = 0)
-
-        self.boundary = np.append(self.boundary, walls + shift, axis = 0)
+        walls += shift
+        
+        #self.spaces = np.append(self.spaces, body, axis = 0)
+        self.spaces = np.vstack((self.spaces, body))
+        
+        #self.boundary = np.append(self.boundary, walls, axis = 0)
+        self.boundary = np.vstack((self.boundary, walls))
         
