Fixed the centroid calculation

tcod_tutorial/map_objects/rooms.py
@@ -19,7 +19,7 @@ class Room:
 
     @property
     def centroid(self):
-        return np.array(self.spaces.shape)/2
+        return (np.array(self.spaces.shape)-1)/2
         
     def mirror_horizontal(self):
         self.spaces = self.spaces[::-1,:]