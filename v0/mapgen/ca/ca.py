fbab504f8dad5eb2b204768d4f8f274d73a892b3
Aldrich Suratos <cyurio@gmail.com>
2019-12-01 21:51:55 +0900

    Starting to move room shapes into their own module

--- v0/mapgen/ca/ca.py
+++ v0/mapgen/ca/ca.py
@@ -8,16 +8,6 @@ class CAMap:
         self.spaces = np.random.randint(0,2,size = (w, h))
         self.count_neighbors_VN()
 
-    def count_neighbors_VN(self):
-        _neighbors = np.sum(np.stack((np.roll(self.spaces, 1, axis = 0),
-                                      np.roll(self.spaces, -1, axis = 0),
-                                      np.roll(self.spaces, 1, axis = 1),
-                                      np.roll(self.spaces, -1, axis = 1))), 
-                                      axis = 0)
-
-        self.neighbors = _neighbors
-        
-
 
     def find_blobs(self):
         pass
