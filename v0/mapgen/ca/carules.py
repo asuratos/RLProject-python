3a52b299633d8ba8862399b5b0fdd62a1c007355
asuratos <asuratos@users.noreply.github.com>
2019-12-03 13:58:05 +0900

    Update carules.py

--- v0/mapgen/ca/carules.py
+++ v0/mapgen/ca/carules.py
@@ -22,15 +22,14 @@ def _count_neighbors_M(space):
 
     return _neighbors
 
-def RuleCave(space):
+def ruleCave(space):
     _neighbors = _count_neighbors_M(space)
     _new = np.zeros_like(space) 
    
-
     # on points where space = 1, apply death rules
-    _new[(space == 1) & (_neighbors > 4)] = 0
+    _new[(space == 1) & (_neighbors < 5)] = 0
 
     #on points where space = 0, apply birth rules
-    _new[(space == 0) & (_neighbors > 2) & (_neighbors < 5)] = 1
+    _new[(space == 0) & (_neighbors > 4)] = 1
     
     return _new
