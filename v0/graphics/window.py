0f16318b8a610f94b93ccdcb43ec4a87417a7001
asuratos <asuratos@users.noreply.github.com>
2019-12-17 12:21:45 +0900

    Building GameWindow class

--- v0/graphics/window.py
+++ v0/graphics/window.py
@@ -1,23 +1,41 @@
-from bearlibterminal import terminal
 from mapgen.dig import Digger
 
 class GameWindow:
-    def __init__(self):
+    def __init__(self, blt):
+        self.blt = blt
+        
         mapwidth = 70
         mapheight = 30
 
-        a = Digger(mapwidth, mapheight, letters = False, floortype = 'default')
-        a.dig_floor(75)
-
-        terminal.open()
-        terminal.set(f'window: size={mapwidth + 20}x{mapheight}, title=RLTest;')
-        terminal.printf(0, 0, str(a))
-        terminal.printf(mapwidth+1, 15, 'Side pane here!')
-        terminal.refresh()
-
-        print(a.roomgraph)
+        self.set_digger(mapwidth, mapheight, 'default', False)
         
-        while terminal.read() != terminal.TK_CLOSE:
-            pass
+        self.set_title(f'window: size={mapwidth + 20}x{mapheight}, title=RLTest;')
         
-        terminal.close()
+        # right now, prints are done with coords relative to the whole window.
+        # should be changed to a set of panels, and prints are assigned relative to 
+        # each panel's position (panel1(0,0) is different from panel2(0,0), etc.)
+        
+        self.blt.printf(0, 0, str(self.floor))
+        self.blt.printf(mapwidth+1, 15, 'Side pane here!')
+        
+        # Initial refresh
+        self.blt.refresh()
+
+        # print(a.roomgraph)
+    
+    def update(self):
+        # update each panel (subwindow) contents
+        
+        # refresh
+        self.blt.refresh()
+        
+    def set_title(self, fstr):
+        self.blt.set(fstr)
+    
+    def set_digger(self, mapwidth, mapheight, floortype = 'default', letters = False, room_attempts = 75):
+        self.floor = Digger(mapwidth, mapheight, letters, floortype)
+        self.floor.dig_floor(room_attempts)
+        
+    def reset_digger(self, room_attempts = 75):
+        self.floor.reset()
+        self.floor.dig_floor(room_attempts)
